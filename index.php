d06b9730e8b9a7326a17092e007fa948efc09e25
Bugra Cuhadaroglu <bugracuhadaroglu@hotmail.com>
2016-03-03 12:40:30 -0500

    Changed field merge name.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <div>
-<a href="http://www.google.com/?elqTrackId=6ACC3D3E374295DF936F1B9ACC93A501&elqTrack=true&myparam=<span class=eloquaemail>First_Name1</span>">Clickme</a>
+<a href="http://www.google.com/?elqTrackId=6ACC3D3E374295DF936F1B9ACC93A501&elqTrack=true&myparam=<span class=eloquaemail>First_Name11</span>">Clickme</a>
 <a href="http://mydomain.com?elqTrack=true">Link 1</a>
 <a href="http://mydomain.com?myparam=val1&elqTrack=true">Link 2</a>
 <a href="http://mydomain.com?elqTrack=true&myFirstName=<span class=eloquaemail>First_Name11</span>">Link 3</a>
